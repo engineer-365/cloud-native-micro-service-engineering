8da2f1308168d5f82e26e5fdbcdd18a9f403205d
Yiting Qiang <qiangyt@wxcount.com>
2020-12-25 16:14:21 +0800

    move infra folder as git submodule

--- infra/virtualbox/boxes/store1/files/opt/mysql-master/create-fleashop.sql
+++
@@ -1,6 +0,0 @@
-CREATE DATABASE fleashop DEFAULT CHARSET utf8mb4 COLLATE utf8mb4_general_ci;
-
-USE fleashop;
-
-CREATE USER 'fleashop_user'@'%' IDENTIFIED BY 'fleashop_password';
-GRANT ALL PRIVILEGES ON fleashop.* TO 'fleashop_user'@'%' WITH GRANT OPTION;
